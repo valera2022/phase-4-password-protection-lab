0a195953d6eff81cd4f7f8a73a769bdc2a2020eb
GeralmyUrena <geralmyuri11@gmail.com>
2023-07-27 10:37:30 -0400

    commit tree 68f2b876148e503a50450901f0dd93432296177d

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+    def create 
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            # byebug
+            render json: user, status: :created
+        else 
+            render json: {error: "wrong credentials"}, status: :unauthorized
+        end
+    
+    end
+
+    def destroy 
+        session.delete :user_id
+        head :no_content    
+    end
+end
